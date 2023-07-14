451da06000bbfee040808169ccb10cf5cb6e51ca
Labelle Candido <labellecandido@icloud.com>
2023-07-14 15:02:42 -0300

    first project config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnCRs_kkxsiCMFYM8uAUodH2Kg0WMK1rg",
+    authDomain: "cardapiodigiital.firebaseapp.com",
+    databaseURL: "https://cardapiodigiital-default-rtdb.firebaseio.com",
+    projectId: "cardapiodigiital",
+    storageBucket: "cardapiodigiital.appspot.com",
+    messagingSenderId: "91519335886",
+    appId: "1:91519335886:web:244dc224437e32b2e13f9f"
+  }
+ };
